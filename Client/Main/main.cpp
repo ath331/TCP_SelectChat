66bc23e759f9a4a6cc93882847ed39900264ee6e
ath331 <kstnftk@naver.com>
2021-02-20 23:54:54 +0900

    Client 솔루션 VS2015로 변경

--- Client/Main/main.cpp
+++ Client/Main/main.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Client Main" << endl;
-	cout << "클라이언트 브랜치 생성" << endl;
-	cout << "클라이언트 테스트" << endl;
+	cout << "ClientMain" << endl;
 
 	return 0;
 }
