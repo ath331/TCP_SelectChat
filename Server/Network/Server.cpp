b9452ea29989f4698c1b092fb4f3d9d1edacb63e
ath331 <kstnftk@naver.com>
2021-02-21 20:32:06 +0900

    Server Class 추가
    Server Class를 만들어서 기능 분리

---
+++ Server/Network/Server.cpp
@@ -0,0 +1,106 @@
+#include "Server.h"
+#include "Timeval.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define BUF_SIZE 1024
+
+void Server::_InitWSA()
+{
+	if (WSAStartup(MAKEWORD(2, 2), &_wsaData) != 0)
+	{
+		cout << "WSAStartup() error" << endl;
+		exit(1);
+	}
+}
+
+void Server::_SettingServer()
+{
+	_Bind();
+	_Listen();
+}
+
+void Server::_Bind()
+{
+	if (bind(_hServSock, (SOCKADDR*)&_servAdr, sizeof(_servAdr)) == SOCKET_ERROR)
+	{
+		cout << "bind() error" << endl;
+		exit(1);
+	}
+}
+
+void Server::_Listen()
+{
+	if (listen(_hServSock, 5) == SOCKET_ERROR)
+	{
+		cout << "listen() error" << endl;
+		exit(1);
+	}
+}
+
+void Server::Run()
+{
+	fd_set reads, cpyReads;
+
+	char buf[BUF_SIZE];
+
+	if (_port == nullptr)
+	{
+		cout << "port is NULL" << endl;
+		exit(1);
+	}
+
+	_hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&_servAdr, 0, sizeof(_servAdr));
+	_servAdr.sin_family = AF_INET;
+	_servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	_servAdr.sin_port = htons(atoi(_port));
+
+	FD_ZERO(&reads);
+	FD_SET(_hServSock, &reads);
+
+	while (true)
+	{
+		cpyReads = reads;
+		Timeval timeval(5, 5000);
+
+		int fdnum = select(0, &cpyReads, 0, 0, timeval.GetPtr());
+		if (fdnum == SOCKET_ERROR)
+			break;
+
+		if (fdnum == 0)
+			continue;
+
+		for (int i = 0; i < reads.fd_count; i++)
+		{
+			if (FD_ISSET(reads.fd_array[i], &cpyReads))
+			{
+				if (reads.fd_array[i] == _hServSock)
+				{
+					int adrSz = sizeof(_clntAdr);
+					_hClntSock = accept(_hServSock, (SOCKADDR*)&_clntAdr, &adrSz);
+					FD_SET(_hClntSock, &reads);
+					cout << "connected clinet : " << _hClntSock << endl;
+				}
+				else
+				{
+					int strLen = recv(reads.fd_array[i], buf, BUF_SIZE - 1, 0);
+					if (strLen == 0)
+					{
+						FD_CLR(reads.fd_array[i], &reads);
+						closesocket(cpyReads.fd_array[i]);
+						cout << "close clinet : " << cpyReads.fd_array[i] << endl;
+					}
+					else
+					{
+						send(reads.fd_array[i], buf, strLen, 0);
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ Server/Network/Server.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <WinSock2.h>
+
+class Server
+{
+public:
+	Server(char* port)
+		:_port(port)
+	{
+		_InitWSA();
+		_SettingServer();
+	}
+	~Server()
+	{
+		closesocket(_hServSock);
+		WSACleanup();
+	}
+
+	void Run();
+
+private:
+	WSADATA _wsaData;
+	SOCKET _hServSock, _hClntSock;
+	SOCKADDR_IN _servAdr, _clntAdr;
+
+	char* _port = nullptr;
+
+private:
+	void _InitWSA();
+
+	void _SettingServer();
+	void _Bind();
+	void _Listen();
+};
+
