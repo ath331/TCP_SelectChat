51c6291b42b63a0edfd92e159019cc7dc13e17e2
ath331 <kstnftk@naver.com>
2021-02-28 04:44:23 +0900

    reset commit

--- Server/Main/main.cpp
+++ Server/Main/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Server.h"
 
 int main(int argc, char* argv[])
@@ -5,5 +6,14 @@ int main(int argc, char* argv[])
 	Server s(argc > 2 ? argv[1] : "9999");
 	s.Run();
 
+=======
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "ServerMain" << endl;
+>>>>>>> parent of 4de941a (Client브랜치에서 서버 프로젝트 삭제)
 	return 0;
 }
