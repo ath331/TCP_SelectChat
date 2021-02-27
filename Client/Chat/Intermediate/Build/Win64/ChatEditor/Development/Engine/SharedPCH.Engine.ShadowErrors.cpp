6b93ffdcb24153c8edc3c40a3d285a62a0525df4
ath331 <kstnftk@naver.com>
2021-02-27 15:34:50 +0900

    기본 프로젝트 생성

---
+++ Client/Chat/Intermediate/Build/Win64/ChatEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/TCP_SelectChat/Client/Chat/Intermediate/Build/Win64/ChatEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Client/Chat/Source/Chat/ChatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAT_API AChatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
